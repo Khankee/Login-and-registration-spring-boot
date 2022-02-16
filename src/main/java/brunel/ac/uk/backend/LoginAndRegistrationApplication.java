a4f5289cda7040184554eb037a5ae6385b665fbb
Khankee <1940930@brunel.ac.uk>
2022-02-16 21:24:33 +0000

    Repo Init

---
+++ src/main/java/brunel/ac/uk/backend/LoginAndRegistrationApplication.java
@@ -0,0 +1,13 @@
+package brunel.ac.uk.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAndRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoginAndRegistrationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/brunel/ac/uk/backend/LoginAndRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package brunel.ac.uk.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAndRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
